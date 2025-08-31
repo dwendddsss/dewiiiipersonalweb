99c68176563c635665c9075ea83e71651355534a
dwendddsss <dewiendng13@gmail.com>
2025-08-31 11:20:04 +0700

    add function wa number

---
+++ script.js
@@ -0,0 +1,6 @@
+const waNumber = "6281358787484";
+const emailAdress = "dewiendng13@gmail.com";
+
+document.getElementById("waBtn").addEventListener("click", function(){
+     window.open(`https://wa.me/${6281358787484}`, "_blank");
+})
