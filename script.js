bd3e91a90d718656b645f7eef8908b83547a1e6a
dwendddsss <dewiendng13@gmail.com>
2025-08-31 11:22:50 +0700

    add email css

--- script.js
+++ script.js
@@ -4,3 +4,7 @@ const emailAdress = "dewiendng13@gmail.com";
 document.getElementById("waBtn").addEventListener("click", function(){
      window.open(`https://wa.me/${6281358787484}`, "_blank");
 })
+
+document.getElementById("emailBtn").addEventListener("click", function(){
+  window.open(`mailto:${dewiendng13@gmail.com}`, "_blank");
+})
